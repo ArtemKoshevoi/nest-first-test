45a65bbd76441394f4462d8009f94c884f9351d2
2mcKosh <2mcKosh@MacBook-Pro.local>
2022-07-04 13:38:33 +0300

    add optional param

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,9 +1,10 @@
 import { PartialType } from '@nestjs/swagger';
-import { IsEnum, ValidateIf } from 'class-validator';
+import { IsEnum, IsOptional, ValidateIf } from 'class-validator';
 import { UserStatus } from '../entities/user.entity';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsEnum(UserStatus)
+  @IsOptional()
   status?: UserStatus;
 }
